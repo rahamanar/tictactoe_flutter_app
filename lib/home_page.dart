44ac3d3ba6baf5fdfdca94b08367de5922795a95
rahamanar <rahamanar123@gmail.com>
2020-11-26 12:39:49 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,219 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:tictactoe_app/custom_dialog.dart';
+import 'package:tictactoe_app/game_button.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<GameButton> buttonlist;
+  var player1;
+  var player2;
+  var activePlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    buttonlist = doInit();
+  }
+
+  List<GameButton> doInit() {
+    player1 = new List();
+    player2 = new List();
+    activePlayer = 1;
+
+    var gameButtons = <GameButton>[
+      GameButton(id: 1),
+      GameButton(id: 2),
+      GameButton(id: 3),
+      GameButton(id: 4),
+      GameButton(id: 5),
+      GameButton(id: 6),
+      GameButton(id: 7),
+      GameButton(id: 8),
+      GameButton(id: 9),
+    ];
+    return gameButtons;
+  }
+
+  void playGame(GameButton gb) {
+    setState(() {
+      if (activePlayer == 1) {
+        gb.text = "X";
+        gb.bg = Colors.red;
+        activePlayer = 2;
+        player1.add(gb.id);
+      } else {
+        gb.text = "O";
+        gb.bg = Colors.black;
+        activePlayer = 1;
+        player2.add(gb.id);
+      }
+      gb.enabled = false;
+      int winner = checkWinner();
+      if (winner == -1) {
+        if (buttonlist.every((element) => element.text != "")) {
+          showDialog(
+              context: context,
+              builder: (_) => CustomDialog("Game Tied",
+                  "Press the reset button to start again.", resetGame));
+        } else {
+          // ignore: unnecessary_statements
+          activePlayer == 2 ? autoPlay() : null;
+        }
+      }
+    });
+  }
+
+  void autoPlay() {
+    var emptyCells = List();
+    var list = List.generate(9, (index) => index + 1);
+    for (var cellID in list) {
+      if (!(player1.contains(cellID) || player2.contains(cellID))) {
+        emptyCells.add(cellID);
+      }
+    }
+
+    var r = new Random();
+    var randIndex = r.nextInt(emptyCells.length - 1);
+    var cellID = emptyCells[randIndex];
+    int i = buttonlist.indexWhere((element) => element.id == cellID);
+    playGame(buttonlist[i]);
+  }
+
+  int checkWinner() {
+    var winner = -1;
+    if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+      winner = 2;
+    }
+    // row 2
+    if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+      winner = 1;
+    }
+    if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+      winner = 2;
+    }
+    // row 3
+    if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+      winner = 2;
+    }
+    //col 1
+    if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+      winner = 2;
+    }
+    // col 2
+    if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+      winner = 1;
+    }
+    if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+      winner = 2;
+    }
+    //col 3
+    if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+      winner = 2;
+    }
+    //diagonal
+    if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+      winner = 2;
+    }
+    if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    if (winner != -1) {
+      if (winner == 1) {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player 1 won",
+                "Press the reset button to start again", resetGame));
+      } else {
+        showDialog(
+            context: context,
+            builder: (_) => CustomDialog("Player 2 won",
+                "Press the reset button to start again", resetGame));
+      }
+    }
+    return winner;
+  }
+
+  void resetGame() {
+    if (Navigator.canPop(context)) Navigator.pop(context);
+    setState(() {
+      buttonlist = doInit();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tic Tac Toe"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 10),
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 9,
+                  mainAxisSpacing: 9),
+              itemCount: buttonlist.length,
+              itemBuilder: (context, index) => SizedBox(
+                width: 100,
+                height: 100,
+                child: RaisedButton(
+                  padding: EdgeInsets.all(10),
+                  onPressed: buttonlist[index].enabled
+                      ? () => playGame(buttonlist[index])
+                      : null,
+                  child: Text(
+                    buttonlist[index].text,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  color: buttonlist[index].bg,
+                  disabledColor: buttonlist[index].bg,
+                ),
+              ),
+            ),
+          ),
+          RaisedButton(
+            padding: EdgeInsets.all(20),
+            onPressed: resetGame,
+            color: Colors.red,
+            child: Text(
+              "Reset",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_app/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.black),
+      home: HomePage(),
+    );
+  }
+}
